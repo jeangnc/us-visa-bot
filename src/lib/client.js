2248a844e9c31d6d3b795742eac5ecf2cfd74ddc
Jean Gonçalves <jean.gnc@gmail.com>
2025-07-20 21:54:02 -0300

    Reorganizes the client

--- src/lib/client.js
+++ src/lib/client.js
@@ -3,6 +3,14 @@ import cheerio from 'cheerio';
 import { log } from './utils.js';
 import { getBaseUri } from './config.js';
 
+// Common headers
+const COMMON_HEADERS = {
+  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Connection': 'keep-alive',
+  'Cache-Control': 'no-store'
+};
+
 export class VisaHttpClient {
   constructor(countryCode, email, password) {
     this.baseUri = getBaseUri(countryCode);
@@ -10,114 +18,135 @@ export class VisaHttpClient {
     this.password = password;
   }
 
+  // Public API methods
   async login() {
     log('Logging in');
 
-    const anonymousHeaders = await fetch(`${this.baseUri}/users/sign_in`, {
-      headers: {
-        "User-Agent": "",
-        "Accept": "*/*",
-        "Accept-Encoding": "gzip, deflate, br",
-        "Connection": "keep-alive",
-      },
-    }).then(response => this.extractHeaders(response));
+    const anonymousHeaders = await this._anonymousRequest(`${this.baseUri}/users/sign_in`)
+      .then(response => this._extractHeaders(response));
 
-    return fetch(`${this.baseUri}/users/sign_in`, {
-      headers: Object.assign({}, anonymousHeaders, {
-        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8",
-      }),
-      method: "POST",
-      body: new URLSearchParams({
-        'utf8': '✓',
-        'user[email]': this.email,
-        'user[password]': this.password,
-        'policy_confirmed': '1',
-        'commit': 'Sign In'
-      }),
-    }).then(res => (
-      Object.assign({}, anonymousHeaders, {
-        'Cookie': this.extractRelevantCookies(res)
-      })
-    ));
+    const loginData = {
+      'utf8': '✓',
+      'user[email]': this.email,
+      'user[password]': this.password,
+      'policy_confirmed': '1',
+      'commit': 'Sign In'
+    };
+
+    return this._submitForm(`${this.baseUri}/users/sign_in`, anonymousHeaders, loginData)
+      .then(res => ({
+        ...anonymousHeaders,
+        'Cookie': this._extractRelevantCookies(res)
+      }));
   }
 
   async checkAvailableDate(headers, scheduleId, facilityId) {
-    return fetch(`${this.baseUri}/schedule/${scheduleId}/appointment/days/${facilityId}.json?appointments[expedite]=false`, {
-      headers: Object.assign({}, headers, {
-        "Accept": "application/json",
-        "X-Requested-With": "XMLHttpRequest",
-      }),
-      cache: "no-store"
-    })
-      .then(r => r.json())
-      .then(r => this.handleErrors(r))
-      .then(d => d.map(item => item.date));
+    const url = `${this.baseUri}/schedule/${scheduleId}/appointment/days/${facilityId}.json?appointments[expedite]=false`;
+    
+    return this._jsonRequest(url, headers)
+      .then(data => data.map(item => item.date));
   }
 
   async checkAvailableTime(headers, scheduleId, facilityId, date) {
-    return fetch(`${this.baseUri}/schedule/${scheduleId}/appointment/times/${facilityId}.json?date=${date}&appointments[expedite]=false`, {
-      headers: Object.assign({}, headers, {
-        "Accept": "application/json",
-        "X-Requested-With": "XMLHttpRequest",
-      }),
-      cache: "no-store",
-    })
-      .then(r => r.json())
-      .then(r => this.handleErrors(r))
-      .then(d => d['business_times'][0] || d['available_times'][0]);
+    const url = `${this.baseUri}/schedule/${scheduleId}/appointment/times/${facilityId}.json?date=${date}&appointments[expedite]=false`;
+    
+    return this._jsonRequest(url, headers)
+      .then(data => data['business_times'][0] || data['available_times'][0]);
   }
 
   async book(headers, scheduleId, facilityId, date, time) {
     const url = `${this.baseUri}/schedule/${scheduleId}/appointment`;
 
-    const newHeaders = await fetch(url, { headers })
-      .then(response => this.extractHeaders(response));
+    const bookingHeaders = await this._anonymousRequest(url, headers)
+      .then(response => this._extractHeaders(response));
 
+    const bookingData = {
+      'utf8': '✓',
+      'authenticity_token': bookingHeaders['X-CSRF-Token'],
+      'confirmed_limit_message': '1',
+      'use_consulate_appointment_capacity': 'true',
+      'appointments[consulate_appointment][facility_id]': facilityId,
+      'appointments[consulate_appointment][date]': date,
+      'appointments[consulate_appointment][time]': time,
+      'appointments[asc_appointment][facility_id]': '',
+      'appointments[asc_appointment][date]': '',
+      'appointments[asc_appointment][time]': ''
+    };
+
+    return this._submitFormWithRedirect(url, bookingHeaders, bookingData);
+  }
+
+  // Private request methods
+  async _anonymousRequest(url, headers = {}) {
     return fetch(url, {
-      method: "POST",
-      redirect: "follow",
-      headers: Object.assign({}, newHeaders, {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      }),
-      body: new URLSearchParams({
-        'utf8': '✓',
-        'authenticity_token': newHeaders['X-CSRF-Token'],
-        'confirmed_limit_message': '1',
-        'use_consulate_appointment_capacity': 'true',
-        'appointments[consulate_appointment][facility_id]': facilityId,
-        'appointments[consulate_appointment][date]': date,
-        'appointments[consulate_appointment][time]': time,
-        'appointments[asc_appointment][facility_id]': '',
-        'appointments[asc_appointment][date]': '',
-        'appointments[asc_appointment][time]': ''
-      }),
+      headers: {
+        "User-Agent": "",
+        "Accept": "*/*",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Connection": "keep-alive",
+        ...headers
+      }
     });
   }
 
-  async extractHeaders(res) {
-    const cookies = this.extractRelevantCookies(res);
+  async _jsonRequest(url, headers = {}) {
+    return fetch(url, {
+      headers: {
+        ...headers,
+        "Accept": "application/json",
+        "X-Requested-With": "XMLHttpRequest"
+      },
+      cache: "no-store"
+    })
+      .then(r => r.json())
+      .then(r => this._handleErrors(r));
+  }
 
+  async _submitForm(url, headers = {}, formData = {}) {
+    return fetch(url, {
+      method: "POST",
+      headers: {
+        ...headers,
+        "Content-Type": "application/x-www-form-urlencoded; charset=UTF-8"
+      },
+      body: new URLSearchParams(formData)
+    });
+  }
+
+  async _submitFormWithRedirect(url, headers = {}, formData = {}) {
+    return fetch(url, {
+      method: "POST",
+      redirect: "follow",
+      headers: {
+        ...headers,
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: new URLSearchParams(formData)
+    });
+  }
+
+  // Private utility methods
+  async _extractHeaders(res) {
+    const cookies = this._extractRelevantCookies(res);
     const html = await res.text();
     const $ = cheerio.load(html);
     const csrfToken = $('meta[name="csrf-token"]').attr('content');
 
     return {
+      ...COMMON_HEADERS,
       "Cookie": cookies,
       "X-CSRF-Token": csrfToken,
       "Referer": this.baseUri,
-      "Referrer-Policy": "strict-origin-when-cross-origin",
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-      'Cache-Control': 'no-store',
-      'Connection': 'keep-alive'
+      "Referrer-Policy": "strict-origin-when-cross-origin"
     };
   }
 
-  extractRelevantCookies(res) {
-    const parsedCookies = this.parseCookies(res.headers.get('set-cookie'));
+  _extractRelevantCookies(res) {
+    const parsedCookies = this._parseCookies(res.headers.get('set-cookie'));
     return `_yatri_session=${parsedCookies['_yatri_session']}`;
   }
 
-  parseCookies(cookies) {
+  _parseCookies(cookies) {
     const parsedCookies = {};
 
     cookies.split(';').map(c => c.trim()).forEach(c => {
@@ -128,7 +157,7 @@ export class VisaHttpClient {
     return parsedCookies;
   }
 
-  handleErrors(response) {
+  _handleErrors(response) {
     const errorMessage = response['error'];
 
     if (errorMessage) {
